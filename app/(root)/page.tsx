dc7b4e8b83e7a1ced04a608fc5fb3aab62ab9cdb
Abhishek Jaglan <jaglanabhishek08@gmail.com>
2024-05-22 23:50:21 +0530

    second commit left sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,41 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
+import TotalBalance from '@/components/TotalBalance'
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = {
+        firstName: "Abhishek",
+        lastName: "Jaglan",
+        email: "jaglanabhishek08@gmail.com"
+    }
   return (
-    <div>Home</div>
+    <section className='home'>
+        <div className='home-content'>
+            <header className='home-header'>
+                <HeaderBox
+                    type="greeting"
+                    title="Welcome"
+                    user={loggedIn?.firstName || "Guest"}
+                    subtext = "Access and manage your account and transactions efficiently"
+                />
+
+                <TotalBalance
+                    accounts={[]}
+                    totalBanks={1}
+                    totalCurrentBalance={1250.35}
+                />
+            </header>
+
+            RECENT TRANSACTION
+        </div>
+
+        <RightSidebar 
+            user={loggedIn}
+            transactions={[]}
+            banks={[{}, {}]}
+        />
+    </section>
   )
 }
 
---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import React from 'react'
+import CountUp from 'react-countup'
+
+const AnimatedCounter = ({amount}:{amount: number}) => {
+  return (
+    <div className='w-full'>
+        <CountUp 
+            end={amount}
+            decimals={2}
+            prefix="₹"
+        />
+    </div>
+  )
+}
+
+export default AnimatedCounter
